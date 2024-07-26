3c7d659b58a50960d9d464a3e81d58c74444f2ed
Vowgan <VowganVR@Gmail.com>
2024-07-26 14:37:59 -0500

    Episode 9 created modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
 import './index.css';
 import Pokemon from './components/Pokemon';
 import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
-    
+
     const [type, setType] = useState("Normal");
+    const [pokemonList, setPokemonList] = useState(
+        [
+            {
+                dex: 252,
+                name: "Treeko",
+                type: "Grass",
+                img: "https://img.pokemondb.net/artwork/alt/treecko-oras.jpg"
+            },
+            {
+                dex: 253,
+                name: "Grovyle",
+                type: "Grass",
+                img: "https://img.pokemondb.net/artwork/large/grovyle.jpg"
+            },
+            {
+                dex: 254,
+                name: "Sceptile",
+                type: "Grass",
+                img: "https://img.pokemondb.net/artwork/large/sceptile.jpg"
+            },
+            {
+                dex: 255,
+                name: "Torchic",
+                type: "Fire",
+                img: "https://img.pokemondb.net/artwork/large/torchic.jpg"
+            },
+            {
+                dex: 256,
+                name: "Combusken",
+                type: "Fire, Fighting",
+                img: "https://img.pokemondb.net/artwork/large/combusken.jpg"
+            },
+            {
+                dex: 257,
+                name: "Blaziken",
+                type: "Fire, Fighting",
+                img: "https://img.pokemondb.net/artwork/large/blaziken.jpg"
+            },
+            {
+                dex: 258,
+                name: "Mudkip",
+                type: "Water",
+                img: "https://img.pokemondb.net/artwork/large/mudkip.jpg"
+            },
+            {
+                dex: 259,
+                name: "Marshtomp",
+                type: "Water, Ground",
+                img: "https://img.pokemondb.net/artwork/large/marshtomp.jpg"
+            },
+            {
+                dex: 260,
+                name: "Swampert",
+                type: "Water, Ground",
+                img: "https://img.pokemondb.net/artwork/large/swampert.jpg"
+            },
+        ]
+    );
 
     const showPokemon = true;
     return (
-        <div className="App bg-red-300">
+        <div className="App">
             {
                 showPokemon ? (
                     <>
                         <input type='text' onChange={(e) => {
                             console.log(e.target.value);
-                            setType(e.target.value);                            
+                            setType(e.target.value);
                         }}/>
-                        <Pokemon name="Treeko" type="Grass"/>
-                        <Pokemon name="Torchic" type={type}/>
-                        <Pokemon name="Mudkip"/>
+                        <div className="flex flex-wrap justify-center">
+                            {pokemonList.map((mon) => {
+                                return (< Pokemon
+                                    key={uuidv4()}
+                                    name={mon.name}
+                                    type={mon.type}
+                                    dex={mon.dex}
+                                    img={mon.img}
+                                />);
+                            })}
+                        </div>
                     </>
                 ) : (
                     <p>You cannot see the Pokemon.</p>
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,13 +1,29 @@
+import EditPokemon from "./EditPokemon";
+
 function Pokemon(props) {
     return (
-        <>
-            <h3>Pokemon {props.name}</h3>
-            {props.type ? (
-                <p className="type">{props.type}</p>
-            ) : (
-                <p className="noType">No Type</p>
-            )}
-        </>
+        <div
+            className=" m-2 py-8 px-8 max-w-sm bg-slate-200 rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            <img
+                className=" object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+                src={props.img}
+                alt="Pokemon preview"/>
+            <div className="text-center space-y-2 sm:text-left">
+                <div className="space-y-0.5">
+                    <p className="text-lg text-black font-semibold">
+                        {props.name}
+                    </p>
+                    <p className="text-slate-500 font-medium">
+                        Type: {props.type}
+                    </p>
+                    <p className="text-slate-500 font-medium">
+                        Dex: {props.dex}
+                    </p>
+                </div>
+                
+                <EditPokemon/>
+            </div>
+        </div>
     )
 }
 
